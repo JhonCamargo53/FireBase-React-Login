c2f76126f63b7a83733f30f5a6c6366cf07690fc
Jhon Camargo <jcamargocelin@gmail.com>
2022-06-02 21:40:00 -0500

    home-page-fixed

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
           <Routes>
 
             <Route path="/" element={<Home />} />
+            <Route path="/FireBase-React-Login" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/requests" element={<RequirementsCRUD />} />\
